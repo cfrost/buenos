e95d215245922f4d2a8c6bf4e47b43ca2fdeb9c9
Christoffer Frost <Christoffer Frost@Frost-Pc>
2013-02-11 09:33:36 +0100

    Tilføjet test

---
+++ tests/test.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   test.h
+ * Author: Christoffer Frost
+ *
+ * Created on 11. februar 2013, 09:32
+ */
+
+#ifndef TEST_H
+#define	TEST_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TEST_H */
+
