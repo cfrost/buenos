1161e00b2424edc701bf0d03434f6f1a31aa2134
Christoffer Frost <Christoffer Frost@Frost-Pc>
2013-02-15 15:02:08 +0100

    readwrite.c rettet

--- tests/readwrite.c
+++ tests/readwrite.c
@@ -3,8 +3,8 @@
 
 int main(void)
 {
-    char buffer[19] = "WRITE IS WORKING \n";
-    syscall_write(FILEHANDLE_STDOUT,buffer,19);
+    char buffer[17] = "WRITE IS WORKING\n";
+    syscall_write(FILEHANDLE_STDOUT,buffer,17);
     
     syscall_halt();
 
